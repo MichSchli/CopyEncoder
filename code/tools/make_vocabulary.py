303993e5a3355d83c305d8bdb86846c60b68ab01
MichSchli <michael.sejr@gmail.com>
2018-12-10 14:25:34 +0000

    stuff

---
+++ code/tools/make_vocabulary.py
@@ -0,0 +1,58 @@
+import argparse
+from tqdm import tqdm
+import numpy as np
+
+parser = argparse.ArgumentParser(description='Train (and test) a model with a given block.')
+parser.add_argument('--sentences')
+parser.add_argument('--embeddings')
+args = parser.parse_args()
+
+extra_tokens = ["<s>", "</s>", "<unk>"]
+embedding_width = 300
+
+word_counts = {}
+
+def tokenize(sentence):
+    return sentence.split(" ")
+
+with open(args.sentences, 'r') as sentences:
+    for sentence in tqdm(list(sentences), desc="Counting words:"):
+        for token in tokenize(sentence.strip()):
+            if token not in word_counts:
+                word_counts[token] = 1
+            else:
+                word_counts[token] += 1
+
+def get_top_n_tokens(count_dictionary, n):
+    sorted_by_value = sorted(count_dictionary.items(), key=lambda kv: kv[1], reverse=True)
+    kept = sorted_by_value[:n]
+    to_return = [kv[0] for kv in kept]
+    return to_return
+
+def get_tokens_mentioned_n_times(count_dictionary, n):
+    return [kv[0] for kv in count_dictionary.items() if kv[1] >= n]
+
+kept_tokens = get_tokens_mentioned_n_times(word_counts, 3)
+all_tokens = extra_tokens + kept_tokens
+
+word_to_index_dict = {w:idx for idx, w in enumerate(all_tokens)}
+
+vocabulary_embeddings = np.random.uniform(-0.01, 0.01, (len(all_tokens), embedding_width)).astype(np.float32)
+
+with open(args.embeddings, 'r') as embeddings:
+    for word_embedding in tqdm(list(embeddings), desc="Reading embeddings:"):
+        parts = word_embedding.split(" ")
+
+        word = parts[0]
+
+        if word in word_to_index_dict:
+            idx = word_to_index_dict[word]
+            vocabulary_embeddings[idx] = np.array(parts[1:], dtype=np.float32)
+
+vocabulary_embeddings = vocabulary_embeddings.astype(np.str)
+
+for i, token in enumerate(all_tokens):
+    if i > 0:
+        print("")
+    print(token, end=" ")
+    print(" ".join(vocabulary_embeddings[i]), end="")
---
+++ train.py
@@ -0,0 +1,23 @@
+import argparse
+
+from Mindblocks.interface import BasicInterface
+
+interface = BasicInterface()
+
+parser = argparse.ArgumentParser(description='Train (and test) a model with a given block.')
+#parser.add_argument('--block')
+args = parser.parse_args()
+
+block_filepath = "blocks/autoencoder.xml"
+block_name = ".".join(block_filepath.split('/')[-1].split(".")[:-1])
+
+data_filepath = "data/toy"
+
+interface.load_file(block_filepath)
+interface.set_variable("data_folder", data_filepath)
+interface.initialize()
+
+for i in range(100):
+    interface.train(1)
+    for line in interface.predict():
+        print(line)
